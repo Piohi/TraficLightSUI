6584016c7db0c978240d01d7c5520838588b260d
Piohi <box1234567b@gmail.com>
2024-04-18 11:24:39 +0300

    Initial Commit

---
+++ TraficLight/TraficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightApp.swift
+//  TraficLight
+//
+//  Created by Anton Godunov on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
